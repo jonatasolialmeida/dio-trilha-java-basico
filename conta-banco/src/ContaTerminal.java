dbe8bdc43b4183ea4dbe72d49339332f8cd0a1cf
jonatas.almeida <jonatas.almeida@globalhitss.com.br>
2023-09-22 01:23:27 -0300

    resolução do desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        
+        // TODO: conhecer e importar a classe Scanner
+        Scanner meuScanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta");
+        numero = meuScanner.nextInt();
+        meuScanner.nextLine();
+
+        System.out.println("Digite o número da agência");
+        agencia = meuScanner.nextLine();
+
+        System.out.println("Digite o seu nome");
+        nomeCliente = meuScanner.nextLine();
+
+        System.out.println("Digite o valor para abertura de conta");
+        saldo = meuScanner.nextDouble();
+        meuScanner.nextLine();
+        // TODO: Exibir as mensagems para o nosso usuário
+        System.out.println("Olá " + nomeCliente + ",obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque");
+
+        // TODO: Exibir a mensagem conta criada
+        System.out.println("Conta Criada com Sucesso!");
+
     }
-}
+} 
