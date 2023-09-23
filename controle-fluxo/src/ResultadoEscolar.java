c3652e601b2936e49d16ecbdbef2da90fdc280b5
jonatas.almeida <jonatas.almeida@globalhitss.com.br>
2023-09-22 21:04:02 -0300

    ternario simples e encadeado

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,13 +2,23 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
-        if(nota >= 7)
-            System.out.println("Aprovado");
+        // if(nota >= 7)
+        //     System.out.println("Aprovado");
             
-        else if (nota >=5 && nota < 7)
-            System.out.println("Prova de recuperação");
+        // else if (nota >=5 && nota < 7)
+        //     System.out.println("Prova de recuperação");
             
-        else
-            System.out.println("Reprovado");
+        // else
+        //     System.out.println("Reprovado");
+        
+        // Operador ternário
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        
+            System.out.println(resultado);
+
+        // Operador ternário encadeado
+        String resultado2 = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Prova de Recuperação" : "Reprovado";
+        
+            System.out.println(resultado2);
     }
 }
